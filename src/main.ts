16a3d0ebc44e7016659976efc39667fb6b6d931a
ALAN MAURICIO <bejaranomauricio999@gmail.com>
2024-10-02 17:24:48 -0400

    se hizo la conexión a la base de datos , tambien se creo el swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('API Rest SIS257_Pizza_House')
+    .setDescription('API Rest del Proyecto de SIS257_Pizza_House')
+    .setVersion('1.0')
+    .addTag('')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('apiSIS257_Pizza_House', app, document);
+
+  await app.listen(process.env.PORT);
+  console.log(`App corriendon ${await app.getUrl()}/apiSIS257_Pizza_House`);
 }
 bootstrap();
